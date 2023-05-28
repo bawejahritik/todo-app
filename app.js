ddaf24ab1e2d69d92f29f21f28fe468c84223713
bawejahritik <bawejahritik@gmail.com>
2023-05-28 12:53:54 +0530

    add ejs template

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.send("Hello")
-})
+    var today = new Date();
+    var day = "";
+    console.log(today.getDay());
+    if(today.getDay() === 6 || today.getDay() === 0){
+        day = "Weekend";
+        res.render("list", {kindOfDay: day});
+    }else{
+        day = "Weekday";
+        res.render("list", {kindOfDay: day});
+    }
+});
 
 app.listen(3000, function(){
-    console.log("Listening on port 3000")
+    console.log("Listening on port 3000");
 });
