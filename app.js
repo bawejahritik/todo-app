bae42a41af6f46d37f68c5df1213be8ec9be4e4d
bawejahritik <bawejahritik@gmail.com>
2023-06-03 03:00:21 +0530

    final push

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items =["first", "second"];
-let workItems = [];
+const items =["first", "second"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    let day = date();
+    const day = date();
     res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work");
@@ -34,7 +34,7 @@ app.get("/work", function(req, res){
 });
 
 app.post("/work", function(req, res){
-    let workItem = req.body.newItem;
+    const workItem = req.body.newItem;
     workItems.push(workItem);
     res.redirect("/work"); 
 });
